ff3533a4cd5718622d7bea6c1f3c933322828e31
lijinke666 <a1231236677287@163.com>
2020-01-15 18:41:38 +0800

    泛型

---
+++ src/Generic.java
@@ -0,0 +1,9 @@
+class MyArrayList<T> {
+    private T[] array;
+    private int size;
+    public void add(T e) {}
+    public void remove(int index) {}
+    public T get(int index) {
+        return this.array[index];
+    }
+}
