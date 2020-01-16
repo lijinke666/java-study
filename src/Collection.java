7988f5cf30fe09b8b4336a168595f0dcefc3a7d2
lijinke666 <a1231236677287@163.com>
2020-01-16 17:14:34 +0800

    feat: 集合

---
+++ src/Collection.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Collection {
+    // 有序列表的集合
+    List<String> list = new ArrayList<>();
+
+    // 没有重复元素的集合, js es6 也实现了这种数据结构
+    Set<String> set = new HashSet<>();
+
+    // 键值对的集合
+    Map<String, String> map = new HashMap<>();
+}
