d759a703089616d859588aa8fd5fc73d66d905d5
lijinke666 <a1231236677287@163.com>
2020-01-20 18:45:27 +0800

    feat: hash map

--- src/Collection.java
+++ src/Collection.java
@@ -1,12 +1,17 @@
 import java.util.*;
 
 public class Collection {
-    // 有序列表的集合
-    List<String> list = new ArrayList<>();
 
-    // 没有重复元素的集合, js es6 也实现了这种数据结构
-    Set<String> set = new HashSet<>();
+    public static void main(String[] args) {
+        // 有序列表的集合
+        List<String> list = new ArrayList<>();
 
-    // 键值对的集合
-    Map<String, String> map = new HashMap<>();
+        // 没有重复元素的集合, js es6 也实现了这种数据结构
+        Set<String> set = new HashSet<>();
+
+        // 键值对的集合
+        Map<String, String> map = new HashMap<>();
+
+        map.put("key","value");
+    }
 }
