27cd3338e061183f5ad085574ac146aa0464ece5
lijinke666 <a1231236677287@163.com>
2020-01-26 12:58:47 +0800

    calendar

--- src/DateClass.java
+++ src/DateClass.java
@@ -5,7 +5,12 @@ public class DateClass {
     public static void main(String[] args) {
         // 获取当前时间:
         Date date = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println(sdf.format(date));
+        System.out.println( new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date));
+
+        // 通过 Calendar 类 来获取时间
+        Calendar c = Calendar.getInstance();
+
+        // getTime 将 calendar 转成 date 类型
+        System.out.println( new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(c.getTime()));
     }
 }
